81f86b07822b5548935efcf75f19514b377afbe2
Max Edelstein <edelsteinm@cmoa.org>
2020-09-08 12:02:25 -0400

    accepting passed values correctly and gives default values if none are given

--- date-range.php
+++ date-range.php
@@ -7,11 +7,19 @@
  * Author: Carnegie Museum of Art
  */
 
- // [date_range startDate="" endDate="" message""]
- function the_actual_code($startDate, $endDate, $message) {
+ // [date_range startdate="" enddate="" message""]
+ function the_actual_code($passedAttributes) {
 
-  // must return, using echo will echo the result hidden in the Gutenberg Editor
-  return 'My name is Jeff';
+   // to give in default values if none are passed
+   $processedAttributes = shortcode_atts( array(
+		'startdate' => 'default 1',
+		'enddate' => 'default 2',
+    'message' => 'default 3',
+	), $passedAttributes );
+
+   // must return, using echo will echo the result hidden in the Gutenberg Editor
+   //return 'startdate is: ' . $passedAttributes["startdate"] . ' enddate is: ' . $passedAttributes["enddate"] . ' message is: ' . $passedAttributes["message"];
+   return 'startdate is: ' . $processedAttributes["startdate"] . ' enddate is: ' . $processedAttributes["enddate"] . ' message is: ' . $processedAttributes["message"];
 
  }
 
